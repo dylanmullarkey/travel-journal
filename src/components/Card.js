902fd4f02036a22aebaab731e038c684d4e78271
Dylan Mullarkey <mullardt@mail.uc.edu>
2024-07-05 00:32:07 -0400

    Layout Card component JSX

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Card(props){
+    return(
+        <div className="card">
+            <img src={`${props.imageUrl}`} className="card--trip-img"></img>
+            <img src="./map-icon.png" className="card--icon-img"></img>
+            <h1 className="card--country">{props.location}</h1>
+            <a href={props.googleMapsUrl} className="card--link">View on Google Maps</a>
+            <h2 className="card--title">{props.title}</h2>
+            <span className="card--dates">{props.startDate} - {props.endDate}</span>
+            <p className="card--description">{props.description}</p>
+        </div>
+    )
+}
