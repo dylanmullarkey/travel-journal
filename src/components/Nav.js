3abd06086fa389c79b0e6454e528e97a22f61811
Dylan Mullarkey <mullardt@mail.uc.edu>
2024-07-04 23:50:20 -0400

    Add Nav JSX

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,11 @@
 import React from "react"
+import "../App.css"
 
 export default function Nav(){
     return(
-        <nav>
-            
+        <nav className="nav--container">
+            <img src="./globe.png" alt="Globe icon" className="globe--icon"/>
+            <h1 className="nav--text">my travel journal.</h1>
         </nav>
     )
 }
