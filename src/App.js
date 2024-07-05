84169a260a2804883fc2b6f54a950a58edc2b800
Dylan Mullarkey <mullardt@mail.uc.edu>
2024-07-04 23:50:07 -0400

    Render nav in app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Nav from "./components/Nav"
 
 export default function App(){
   return(
-    <div>
-
+    <div className='app--container'>
+      <Nav />
     </div>
   )
 }
