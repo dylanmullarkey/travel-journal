30ca2b7bcb5a1cf5acf23697dcc8963e741478ae
Dylan Mullarkey <mullardt@mail.uc.edu>
2024-07-05 19:47:46 -0400

    Add div to card for flexbox

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,12 +4,17 @@ export default function Card(props){
     return(
         <div className="card">
             <img src={`${props.imageUrl}`} className="card--trip-img"></img>
-            <img src="./map-icon.png" className="card--icon-img"></img>
-            <h1 className="card--country">{props.location}</h1>
-            <a href={props.googleMapsUrl} className="card--link">View on Google Maps</a>
-            <h2 className="card--title">{props.title}</h2>
-            <span className="card--dates">{props.startDate} - {props.endDate}</span>
-            <p className="card--description">{props.description}</p>
+            <div className="card--info-container">
+                <div>
+                    <img src="./map-icon.png" className="card--icon-img"></img>
+                    <h1 className="card--country">{props.location}</h1>
+                    <a href={props.googleMapsUrl} className="card--link">View on Google Maps</a>
+                </div>
+                <h2 className="card--title">{props.title}</h2>
+                <span className="card--dates">{props.startDate} - {props.endDate}</span>
+                <p className="card--description">{props.description}</p>
+            </div>
+
         </div>
     )
 }
