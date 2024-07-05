053e800ec06c03c70dfe58a7577a4c8608ac09f7
Dylan Mullarkey <mullardt@mail.uc.edu>
2024-07-05 00:31:43 -0400

    Render card component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from 'react'
 import Nav from "./components/Nav"
+import Card from "./components/Card"
+import data from "./data"
+
 
 export default function App(){
+  console.log(data)
+  const cards = data.map(card => {
+    return <Card 
+            key={card.id}
+            {...card}
+            />
+  })
+
   return(
     <div className='app--container'>
       <Nav />
+      <main className='cards--container'>
+        {cards}
+      </main>
     </div>
   )
 }
